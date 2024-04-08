dcf9da52e93cd6e8606d0217ae698fd63f3e24a3
jeevithakannan2 <ksjeevithakannan123@gmail.com>
2024-04-08 14:16:18 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const { asyncFunction } = require('./amazon');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+    console.log(`Request from: ${req.ip}:${port}`);
+    next();
+  });
+  
+app.post('/run', (req, res) => {
+    const { link } = req.body;
+
+    if (!link) {
+        return res.status(400).json({'Error': 'Link is required'});
+    }
+    
+    asyncFunction(link).then(result => {    
+        console.log(result);
+        res.status(200).json(result);
+    }).catch(err => {
+        console.error(err);
+        res.status(500).json({'Error': 'Internal Server Error'});
+    });
+});
+
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Server is listening at port :${port}`);
+});
